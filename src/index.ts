4c571b711c94e82004cfbaacfef5bfffa9a6a723
Joruno-w <1710642275@qq.com>
2023-04-27 16:16:21 +0800

    feat: 修改目录

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,32 @@
 import fs from "node:fs";
 import http from "isomorphic-git/http/node";
 import git from "isomorphic-git";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { execSync } from "node:child_process";
 
-const path = "../zhuanzhuan";
+const __dirname = resolve(fileURLToPath(import.meta.url), '../');
 
-const config = {
+const config: Record<string, string> = {
   zlj_price_comparison_h5: "zlj_price_comparison_h5-feature-2879-26",
 };
 
-const version = JSON.parse(
-  fs.readFileSync(`${path}/b2c_public_components/package.json`, "utf-8")
+const version: string = JSON.parse(
+  fs.readFileSync(`${__dirname}/b2c_public_components/package.json`, "utf-8")
 ).version;
 
-const pullBranch = async (dir: string, branchName: string): Promise<void> => {
+const author: { name: string; email: string } = (() => {
+  let name;
+  let email;
   try {
-    await git.pull({
-      fs,
-      http,
-      dir,
-      ref: branchName,
-      singleBranch: true,
-    });
-    await git.pull({
-      fs,
-      http,
-      dir,
-      ref: "master",
-      singleBranch: true,
-    });
-  } catch (e) {
-    console.error("代码有冲突哦!");
-  }
-};
+    name = execSync("git config --global user.name");
+    email = execSync("git config --global user.email");
+  } catch (e) {}
+  return {
+    name: name?.toString("utf-8").replace(/\s/g, "") || "",
+    email: email?.toString("utf-8").replace(/\s/g, "") || "",
+  };
+})();
 
 const createBranch = async (dir: string, branchName: string) => {
   await git.checkout({
@@ -78,7 +73,7 @@ const processBranches = async (dir: string, branchName: string) => {
       });
     } else {
       if (!remoteBranches.includes(branchName)) {
-        console.error("你还没在beetle上创分支哦～");
+        console.error(`你还没在beetle上创建 ${branchName} 分支哦～`);
       } else {
         createBranch(dir, branchName);
       }
@@ -93,6 +88,7 @@ const getKitVersion = async (dir: string, kitName: string) => {
 };
 
 const setKitVersion = async (dir: string, kitName: string, version: string) => {
+  if ((await getKitVersion(dir, kitName)) === version) return;
   const pkgPath = `${dir}/package.json`;
   const pkg = JSON.parse(fs.readFileSync(pkgPath, "utf-8"));
   pkg.dependencies[kitName] = version;
@@ -101,31 +97,34 @@ const setKitVersion = async (dir: string, kitName: string, version: string) => {
 
 const pushBranches = async (dir: string, branchName: string) => {
   await git.add({ fs, dir, filepath: "." });
-  let sha = await git.commit({
+  await git.commit({
     fs,
     dir,
-    author: {
-      name: "Mr. Test",
-      email: "mrtest@example.com",
-    },
-    message: "Added the a.txt file",
+    author,
+    message: "feat: 升级组件库",
   });
-  const pushResult = await git.push({
+  await git.push({
     fs,
     http,
     dir,
     remote: "origin",
     ref: branchName,
+    onAuth: () => ({ username: "oauth2", password: "xGh_PG8cbgTNwyg5WPhT" }),
   });
-  console.log(pushResult);
 };
 
-Object.entries(config).forEach(async ([projectName, branchName]) => {
-  const dir = `${path}/${projectName}`;
-  // 处理分支
-  await processBranches(dir, branchName);
-  // 修改版本
-  await setKitVersion(dir, "@zz-yp/b2c-ui", version);
-  // 推送分支
-  // await pushBranches(dir, branchName);
-});
+const init = () => {
+  Object.entries(config).forEach(async ([projectName, branchName]) => {
+    const dir = `${__dirname}/${projectName}`;
+    // 处理分支
+    await processBranches(dir, branchName);
+    // 修改版本
+    await setKitVersion(dir, "@zz-yp/b2c-ui", version);
+    // 推送分支
+    await pushBranches(dir, branchName);
+  });
+};
+
+export default init;
+
+init();
