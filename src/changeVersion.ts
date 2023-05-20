c87d1c75fc1498d83a81b784ff8dd403e07a870b
Joruno-w <1710642275@qq.com>
2023-05-20 14:13:09 +0800

    feat: change readme

--- src/changeVersion.ts
+++ src/changeVersion.ts
@@ -4,5 +4,5 @@ import { resolve } from 'node:path'
 const pkgPath = resolve(fileURLToPath(import.meta.url), '../../package.json')
 const readmePath = resolve(fileURLToPath(import.meta.url), '../../README.md')
 const { version } = JSON.parse(await readFile(pkgPath, 'utf-8'))
-const result = (await readFile(readmePath, 'utf-8')).replace(/^-v(.+)-$/, version)
+const result = (await readFile(readmePath, 'utf-8')).replace(/(\d+\.\d+\.\d+)/g, version)
 await writeFile(readmePath, result, 'utf-8')
