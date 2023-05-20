916b8909a150ae62d5278acf091cf6c77611f2c3
Joruno-w <1710642275@qq.com>
2023-05-20 14:04:01 +0800

    feat: add changeversion script

---
+++ src/changeVersion.ts
@@ -0,0 +1,8 @@
+import { fileURLToPath } from 'node:url'
+import { readFile, writeFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+const pkgPath = resolve(fileURLToPath(import.meta.url), '../../package.json')
+const readmePath = resolve(fileURLToPath(import.meta.url), '../../README.md')
+const { version } = JSON.parse(await readFile(pkgPath, 'utf-8'))
+const result = (await readFile(readmePath, 'utf-8')).replace(/^-v(.+)-$/, version)
+await writeFile(readmePath, result, 'utf-8')
