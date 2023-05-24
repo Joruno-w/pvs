57cd40140744308288f2c59be2739c4b5d3c827e
Joruno <54349117+Joruno-w@users.noreply.github.com>
2023-05-24 20:44:23 +0800

    feat: add detail log

--- src/cli.ts
+++ src/cli.ts
@@ -19,7 +19,7 @@ const getFullPath = async (name: string) => {
 // 安装新版组件库
 const install = (projectPath: any, name: string, version: any) => {
   const agent = existsSync(`${projectPath}/pnpm-lock.yaml`) ? 'pnpm' : 'npm'
-  spinner = ora('正在安装新版组件库...').start()
+  spinner = ora(`${branch} 正在安装新版组件库...`).start()
   const res = shell.exec(`cd ${projectPath} && ${agent} i ${name}@${version}`, {
     silent: true,
     async: true,
@@ -28,9 +28,9 @@ const install = (projectPath: any, name: string, version: any) => {
 }
 
 // 更新所有工程的组件库版本
-const updateProjectVersion = (projectPath: string, branch: unknown, version: string) => {
+const updateProjectVersion = (projectPath, branch, version) => {
   // 切换到指定分支
-  if (shell.exec(`cd ${projectPath} && git checkout ${branch}`).code !== 0) {
+  if (shell.exec(`cd ${projectPath} && git fetch --all && git checkout ${branch}`).code !== 0) {
     shell.echo(c.red('切换分支出错'))
     shell.exit(1)
   }
@@ -41,7 +41,7 @@ const updateProjectVersion = (projectPath: string, branch: unknown, version: str
   })
   if (statusStdout.length > 0) {
     shell.echo(
-      c.red('Git当前工作区状态不是 clean，请确认！或者通过加 GIT_CHECK=none 环境变量跳过检查！'),
+      c.red('Git当前工作区状态不是 clean，请确认！或者通过加 GIT_CHECK=none 环境变量跳过检查！')
     )
     shell.exit(1)
   }
@@ -58,26 +58,23 @@ const updateProjectVersion = (projectPath: string, branch: unknown, version: str
   }
 
   // 安装新版组件库
-  const { stdout } = install(projectPath, '@zz-yp/b2c-ui', version)
+  const { stdout } = install(projectPath, '@zz-yp/b2c-ui', version, branch)
   let flag = true
-  if (!stdout)
-    shell.exit(1)
-  stdout.on('data', (data) => {
+  stdout.on('data', function (data) {
     if (data.includes('@zz-yp/b2c-ui') && flag) {
       flag = false
-      spinner && spinner.succeed(c.green('安装完毕'))
+      spinner.succeed(c.green(`${branch} 安装完毕`))
       // 推送
       const { stdout: st } = shell.exec(
-        `cd ${projectPath} && git add . && git commit -m"feat: 升级组件库" && git push`,
+        `cd ${projectPath} && git add . && git commit -m"feat: 升级组件库@${version}" && git push`,
         {
           silent: true,
           async: true,
-        },
+        }
       )
-      if (!st)
-        shell.exit(1)
+      if (!st) shell.exit(1)
       st.on('data', () => {
-        shell.echo(c.green('推送成功!'))
+        shell.echo(c.green(`${branch} 推送成功!`))
       })
     }
   })
@@ -101,30 +98,40 @@ const valiate = () => {
 // 版本同步(pkg version sync)
 const pvs = async () => {
   valiate()
-  const { version, branches } = pkg as any
+  const { version, branches } = pkg
   const { upgrade } = await prompts({
     type: 'confirm',
     name: 'upgrade',
-    message: '你确定要更新关联的所有工程吗?',
+    message: `你确定要更新所有关联的工程吗? 更新结束后会自动提交。\n${c.inverse(
+      branches.join('\n')
+    )}`,
   })
-  if (!upgrade || !branches)
-    process.exit(0)
-  for (const [project, branch] of Object.entries(branches)) {
-    if (!branch)
+  if (!upgrade || !branches) process.exit(0)
+  for (const branch of branches) {
+    const matches = branch.match(/(\w+)-feature-\d{4}-\d{1,}/)
+    const project = matches ? matches[1] : ''
+    if (!branch || !project) {
+      shell.echo(c.red(`🦄️ 分支 ${branch} 命名不合法，已跳过更新流程`))
       continue
+    }
     const projectPath = await getFullPath(project)
-    if (!projectPath)
+    if (!projectPath) {
+      shell.echo(
+        c.red(
+          `🦄️ 工程 ${project} 不存在，请确保工程与b2c_public_components在同一个目录下，已跳过更新流程`
+        )
+      )
       continue
+    }
     const { dependencies } = JSON.parse(
-      await fs.readFile(`${projectPath}/package.json`, { encoding: 'utf-8' }),
+      await readFile(`${projectPath}/package.json`, { encoding: 'utf-8' })
     )
     const b2cUiVersion = dependencies['@zz-yp/b2c-ui']
-    if (version.replace('^', '') === b2cUiVersion.replace('^', '')) {
-      shell.echo(c.green(`🦄️ 在${branch}分支上,@zz-yp/b2c-ui已升级到了最新版本,跳过更新流程`))
+    if (version.replace(/(^|~)/, '') === b2cUiVersion.replace(/(^|~)/, '')) {
+      shell.echo(c.red(`🦄️ 在 ${branch} 分支上，@zz-yp/b2c-ui已升级到了最新版本，已跳过更新流程`))
       continue
     }
     updateProjectVersion(projectPath, branch, version)
   }
 }
-
 export default pvs
